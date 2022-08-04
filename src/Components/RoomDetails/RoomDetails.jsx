c13fa752ff78e19e58587e1f12780ab33e0d3453
Babalola Muhammad <barmilitary@yahoo.com>
2022-08-04 17:48:42 +0100

    fixed

--- src/Components/RoomDetails/RoomDetails.jsx
+++ src/Components/RoomDetails/RoomDetails.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { TbCurrencyNaira } from 'react-icons/tb';
 
 const RoomDetail = ({ item }) => {
-    const [index, setIndex] = useState(0);
     const {_id, title, price, images, desc } = item;
   
     return(
