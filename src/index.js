e82cbe7c6295d8023af91b5aa468478a956495e1
Babalola Muhammad <barmilitary@yahoo.com>
2022-10-23 20:00:15 +0100

    fixed color

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import { BrowserRouter as Router} from 'react-router-dom'
 
 import App from "./App";
 import { SearchContextProvider } from "./contexts/SearchContext";
-import { AuthContextProvider } from "./contexts/AuthContext";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-        <AuthContextProvider>
             <SearchContextProvider>
                 <Router>
                     <App />
                 </Router>
             </SearchContextProvider>
-        </AuthContextProvider>
 )
